14b208e7e57e9da2703513262059e126d290a693
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-18 12:28:04 -0500

    same-frequency and two-array-object done

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,81 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+
+const createFrequencyObj = (string) => {
+  const obj = {};
+  for (let char of string) {
+    obj[char]
+      ? obj[char] = obj[char] + 1
+      : obj[char] = 1
+  }
+  // console.log(obj)
+  return obj
 }
+
+
+
+
+function sameFrequency(numOne, numTwo) {
+  const numStringOne = String(numOne)
+  const numStringTwo = String(numTwo)
+
+  const numObjOne = createFrequencyObj(numStringOne)
+  const numObjTwo = createFrequencyObj(numStringTwo)
+
+  console.log(numObjOne)
+  console.log(numObjTwo)
+
+
+  for (let key in numObjOne) {
+    if (!numObjTwo[key]) {
+      console.log(false)
+      return false
+    } else if (numObjOne[key] !== numObjTwo[key]) {
+      console.log(false)
+      return false
+    } else {
+      console.log(true)
+      return true
+    }
+
+
+    // console.log(key)
+    // console.log(numObjTwo[key])
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sameFrequency(182, 281)
+console.log(true)
+console.log('################')
+// true
+
+sameFrequency(34, 14)
+console.log(false)
+console.log('################') 
+// false
+
+sameFrequency(3589578, 5879385)
+console.log(true)
+console.log('################') 
+// true
+
+sameFrequency(22, 222)
+console.log(false)
+console.log('################') 
+// false
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,35 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arrOne, arrTwo) {
+
+  const obj = {}
+  for (let i=0; i<arrOne.length; i++) {
+    if (i<arrTwo.length) {
+      obj[arrOne[i]] = arrTwo[i] 
+    } else {
+      obj[arrOne[i]] = null
+    }
+  }
+  console.log(obj)
+  return obj
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3])
+// {'a': 1, 'b': 2, 'c': 3, 'd': null}
+
+twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4])
+// {'a': 1, 'b': 2, 'c': 3}
+
+twoArrayObject(['x', 'y', 'z'], [1, 2])
+// {'x': 1, 'y': 2, 'z': null}
